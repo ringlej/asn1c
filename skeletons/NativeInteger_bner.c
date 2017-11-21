7f54ee0266b6ca7f8375ca256e8739d9a96969fb
Jon Ringle <jringle@gridpoint.com>
2017-11-21 01:30:47 -0500

    BNER: Add NativeInteger_decode_bner code

--- skeletons/NativeInteger_bner.c
+++ skeletons/NativeInteger_bner.c
@@ -5,24 +5,45 @@
  */
 #include <NativeInteger.h>
 #include <asn_internal.h>
+#include <bner_primitive.h>
 
 asn_dec_rval_t
 NativeInteger_decode_bner(const asn_codec_ctx_t *opt_codec_ctx,
                           const asn_TYPE_descriptor_t *td, void **nint_ptr,
                           const void *buf_ptr, size_t size, ber_tlv_tag_t tag,
                           int tag_mode) {
-    (void)opt_codec_ctx;
-    (void)td;
-    (void)nint_ptr;
-    (void)buf_ptr;
-    (void)size;
-    (void)tag;
-    (void)tag_mode;
+    const asn_INTEGER_specifics_t *specs =
+        (const asn_INTEGER_specifics_t *)td->specifics;
+    INTEGER_t *integer_tmp = 0;
+    long *native = (long *)*nint_ptr;
+    asn_dec_rval_t rval;
+    long l;
 
-    asn_dec_rval_t tmp_error = {RC_FAIL, 0};
-    ASN_DEBUG("%s Not yet implemented. Failed to decode %s", __func__,
-              td->name);
-    return tmp_error;
+    ASN_DEBUG("Decoding %s as Native INTEGER (tm=%d)", td->name, tag_mode);
+    rval = bner_decode_primitive(opt_codec_ctx, td, (void **)&integer_tmp,
+                                 buf_ptr, size, tag, tag_mode);
+
+    if(rval.code != RC_OK) return rval;
+
+    /*
+     * If the structure is not there, allocate it.
+     */
+    if(native == NULL) {
+        native = (long *)(*nint_ptr = CALLOC(1, sizeof(*native)));
+        if(native == NULL) ASN__DECODE_FAILED;
+    }
+
+    if((specs && (specs->field_unsigned || specs->strict_enumeration))
+           ? asn_INTEGER2ulong(integer_tmp, (unsigned long *)&l)
+           : asn_INTEGER2long(integer_tmp, &l)) {
+        rval.code = RC_FAIL;
+        rval.consumed = 0;
+        return rval;
+    }
+
+    *native = l;
+
+    return rval;
 }
 
 asn_enc_rval_t
