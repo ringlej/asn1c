5a9733b8fc26c437694819ae62fe0e2825e2f640
Jon Ringle <jringle@gridpoint.com>
2017-11-30 21:29:41 -0500

    WIP

--- skeletons/NativeReal_bner.c
+++ skeletons/NativeReal_bner.c
@@ -13,7 +13,30 @@ NativeFloat_decode_bner(const asn_codec_ctx_t *opt_codec_ctx,
                         const asn_TYPE_descriptor_t *td, void **float_ptr,
                         const void *buf_ptr, size_t size, ber_tlv_tag_t tag,
                         int tag_mode) {
-    (void)opt_codec_ctx;
+	REAL_t *real_tmp = 0;
+	float *f = (float *)*float_ptr;
+	asn_dec_rval_t rval;
+
+	/*
+	 * If the structure is not there, allocate it.
+	 */
+	if(f == NULL) {
+		*float_ptr = CALLOC(1, sizeof(*f));
+		f = (float *)*float_ptr;
+		if(f == NULL) {
+			rval.code = RC_FAIL;
+			rval.consumed = 0;
+			return rval;
+		}
+	}
+
+	ASN_DEBUG("Decoding %s as Native REAL (tm=%d)",
+		td->name, tag_mode);
+
+	rval = bner_decode_primitive(opt_codec_ctx, td, (void **)&real_tmp,
+                                 buf_ptr, size, tag, tag_mode);
+
+	(void)opt_codec_ctx;
     (void)td;
     (void)float_ptr;
     (void)buf_ptr;
